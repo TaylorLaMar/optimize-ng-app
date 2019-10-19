268154b823fb11308dbb8948c5848f18d081fcc3
Taylor LaMar <taylorlamar147@gmail.com>
2019-10-19 02:36:40 -0700

    Make memoization

--- src/app/fibonacci.pipe.ts
+++ src/app/fibonacci.pipe.ts
@@ -2,12 +2,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 import { fibonacci } from './common/utils';
 
+import memo from 'memo-decorator';
+
 @Pipe({
   name: 'fibonacci',
   pure: true
 })
 export class FibonacciPipe implements PipeTransform {
+  @memo()
   transform(value: any, ...args: any[]): any {
+    console.log('value', value);
     return fibonacci(value);
   }
 }
